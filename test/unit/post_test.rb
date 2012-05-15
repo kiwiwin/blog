ce5adae507d53f2e575b269a2a02d0b3ea0704de
kiwi <kiwi.swhite.coder@gmail.com>
2012-05-15 10:13:20 +0800

    add some post unit testcase

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "valid post" do
+		post = Post.new
+		post.name = "dummy name"
+		post.title = "tile is more than 5 characters"
+		post.content = "dummy content"
+		assert post.valid?
+	end
 
 	test "post attributes must not be empty" do
 		post = Post.new
@@ -11,5 +15,16 @@ class PostTest < ActiveSupport::TestCase
 		assert post.errors[:name].any?
 		assert post.errors[:title].any?
 		assert post.errors[:content].any?
+	end
+
+	test "post title length must not less than 5" do
+		post = Post.new
+		post.name = "dummy name"
+		post.title = "abc"
+		post.content = "dummy content"
+		assert post.invalid?
+		assert post.errors[:name].empty?
+		assert post.errors[:content].empty?
+		assert_equal post.errors[:title], ["is too short (minimum is 5 characters)"]
 	end
 end
