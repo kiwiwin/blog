04d49147973b4f1c880822f67df7d7171cbd456e
kiwi <kiwi.swhite.coder@gmail.com>
2012-05-15 10:17:55 +0800

    add comment unit testcase

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :commenter
+
+	validates :body, :commenter, presence: true
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "valid comment" do
+		comment = Comment.new
+		comment.commenter = "dummy commenter"
+		comment.body = "dummy body"
+		assert comment.valid?
+	end
+
+	test "comment attributes must not be empty" do
+		comment = Comment.new
+		assert comment.invalid?
+		assert comment.errors[:commenter].any?
+		assert comment.errors[:body].any?
+	end
 end
